5a077c70e377d4d42d86eb23a3ba8d08b8d2c23a
AlexRod97 <rodriguezalex86@hotmail.com>
2018-03-28 10:19:35 -0600

    ArbolB class implemented

---
+++ EDDProyecto/ArbolB/ITextoTamañoFijo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolB
+{
+    public interface ITextoTamañoFijo
+    {
+        int FixedSizeText { get; }
+        string ToFixedSizeString();
+    }
+}
---
+++ EDDProyecto/EDDProyecto/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDDProyecto.Models
+{
+    public class Usuario : IComparable
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
